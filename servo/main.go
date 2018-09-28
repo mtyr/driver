d96f3fb43a6de9a71f37a2603bbf9efb97168402
makki0205 <llxo2_5oxll@icloud.com>
2018-09-28 09:38:00 +0900

    .

--- servo/main.go
+++ servo/main.go
@@ -4,12 +4,25 @@ import (
 	"fmt"
 
 	"github.com/gin-gonic/gin"
+	rpio "github.com/stianeikeland/go-rpio"
 )
 
 func main() {
+	err := rpio.Open()
+	if err != nil {
+		panic(err)
+	}
+	pin := rpio.Pin(10)
+	pin.Output()
 	r := gin.Default()
 	r.GET("/:deg", func(c *gin.Context) {
-		fmt.Println(c.Param("deg"))
+		deg := c.Param("deg")
+		fmt.Println(deg)
+		if deg == "on" {
+			pin.High()
+		} else {
+			pin.Low()
+		}
 		c.JSON(200, "ok!")
 	})
 	r.Run(":8000")
